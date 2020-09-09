fcd15c2f29bce2f7d72d4b6edb69f0a764f01c8e
Binamra <me.binamra@gmail.com>
2020-09-09 13:57:03 +0545

    const

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 fs.existsSync("public/uploads") || fs.mkdirSync("public/uploads");
-
+console.log('folder created');
 app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
